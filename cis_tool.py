f038446357efb3f530cc4900dd4b9a1f42938523
cabotwang <58140481+cabotwang@users.noreply.github.com>
2022-05-18 19:39:14 +0800

    Update cis_tool.py

--- cis_tool.py
+++ cis_tool.py
@@ -13,9 +13,9 @@ class cistoolApp(HydraHeadApp):
 
         @st.cache
         def data_read():
-            drug_utl = pd.read_csv('./base_data/incidence_rate.csv', usecols=['商品名', '适应症', '治疗评级', '使用率'])
-            drug_cost = pd.read_csv('./base_data/drug_cost_peryear.csv', usecols=['商品名', '通用名', '适应症', '人均费用'])
-            full_data = pd.read_csv('./base_data/full_data.csv', usecols=['唯一识别号', '地区', '适应症', '商品名', '通用名',
+            drug_utl = pd.read_csv('incidence_rate.csv', usecols=['商品名', '适应症', '治疗评级', '使用率'])
+            drug_cost = pd.read_csv('drug_cost_peryear.csv', usecols=['商品名', '通用名', '适应症', '人均费用'])
+            full_data = pd.read_csv('full_data.csv', usecols=['唯一识别号', '地区', '适应症', '商品名', '通用名',
                                                                           '人次数', '药品总金额', '本次赔付金额', '既往症人数',
                                                                           '1万判定', '1.5万判定', '2万判定'])
             region_info = pd.read_csv('./base_data/region_info.csv', usecols=['地区', '总参保人数', '非既往症', '既往症', '免赔额'])
